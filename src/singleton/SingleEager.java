1f0616b3e31d9e075dba57ec1de40b5425ff22db
yanggz <yanggz@ifeng.com>
2020-09-14 15:28:18 +0800

    rename eager

---
+++ src/singleton/SingleEager.java
@@ -0,0 +1,13 @@
+package singleton;
+
+/**
+ * 饿汉模式
+ */
+public class SingleEager {
+
+    private static final SingleEager single = new SingleEager();
+
+    public static SingleEager getInstance() {
+        return single;
+    }
+}
