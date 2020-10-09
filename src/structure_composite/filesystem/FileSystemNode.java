8743e5c44ed85bedb0708e4ea9eae4e136b1881e
yanggz <yanggz@ifeng.com>
2020-10-09 15:17:01 +0800

    add composite

---
+++ src/structure_composite/filesystem/FileSystemNode.java
@@ -0,0 +1,16 @@
+package structure_composite.filesystem;
+
+public abstract class FileSystemNode {
+    protected String path;
+
+    public FileSystemNode(String path) {
+        this.path = path;
+    }
+
+    public abstract int countNumOfFiles();
+    public abstract long countSizeOfFiles();
+
+    public String getPath() {
+        return path;
+    }
+}
---
+++ src/structure_composite/humanresource/Employee.java
@@ -0,0 +1,14 @@
+package structure_composite.humanresource;
+
+
+public class Employee extends HumanResource {
+    public Employee(long id, double salary) {
+        super(id);
+        this.salary = salary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary;
+    }
+}
