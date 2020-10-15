f97066d5b969ef64bfaaaef7b1e4b5e7f5965447
yanggz <yanggz@ifeng.com>
2020-10-15 13:25:11 +0800

    add template

---
+++ src/behavior_template/AbstractClass.java
@@ -0,0 +1,15 @@
+package behavior_template;
+
+public abstract class AbstractClass {
+
+    public final void templateMethod() {
+        //...
+        method1();
+        //...
+        method2();
+        //...
+    }
+
+    protected abstract void method1();
+    protected abstract void method2();
+}
---
+++ src/behavior_template/ConcreteClass2.java
@@ -0,0 +1,13 @@
+package behavior_template;
+
+public class ConcreteClass2 extends AbstractClass {
+    @Override
+    protected void method1() {
+        //...
+    }
+
+    @Override
+    protected void method2() {
+        //...
+    }
+}
