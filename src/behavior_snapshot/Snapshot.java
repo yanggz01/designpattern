a42ce93be3a611b1de53614afcb2d20fad50d75e
yanggz <yanggz@ifeng.com>
2020-11-03 13:51:50 +0800

    add snapshot

---
+++ src/behavior_snapshot/Snapshot.java
@@ -0,0 +1,13 @@
+package behavior_snapshot;
+
+public class Snapshot {
+    private String text;
+
+    public Snapshot(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}
