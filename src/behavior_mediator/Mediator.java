8ffc44690c12c09f02f2d07b1f780f21fb565031
yanggz <yanggz@ifeng.com>
2020-11-06 13:45:58 +0800

    add mediator

---
+++ src/behavior_mediator/Mediator.java
@@ -0,0 +1,7 @@
+package behavior_mediator;
+
+import java.awt.*;
+
+public interface Mediator {
+    void handleEvent(Component component, String event);
+}
---
+++ src/behavior_mediator/Selection.java
@@ -0,0 +1,7 @@
+package behavior_mediator;
+
+public class Selection {
+    public String select() {
+        return null;
+    }
+}
